979475c03a178842c1f9780b2201330d6a1cbead
Adebisea <abdulgkhadijah@gmail.com>
2025-06-13 14:31:52 +0100

    updated globals to include mocha

--- App/eslint.config.mjs
+++ App/eslint.config.mjs
@@ -6,5 +6,12 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs}"], languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      }
+    }
+  },
 ]);
