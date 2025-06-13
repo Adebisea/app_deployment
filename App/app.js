5caa26591e49d7df099fa3c22ee7a16b6edbe41d
Adebisea <abdulgkhadijah@gmail.com>
2025-06-13 10:25:59 +0100

    updated app ext

---
+++ App/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/static/';
+const port = 3000;
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
